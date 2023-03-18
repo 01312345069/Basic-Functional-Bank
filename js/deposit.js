8695aaddd029690c283cc1196cab4c7978e470ee
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2023-03-18 22:31:24 +0600

    deposit and balance and handle string and others added

--- js/deposit.js
+++ js/deposit.js
@@ -3,13 +3,31 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     // step - 2: get the deposit amount from the deposit input field 
     // for input field use .value to the value inside the input field 
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const NweDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(NweDepositAmountString);
 
     // step - 3: get the current deposit total 
     // for non-input (Element other than input, textarea) use innerText to get the text 
-    const depositTotalEelemet = document.getElementById('deposit-total');
-    const depositTotal = depositTotalEelemet.innerText;
-    depositTotalEelemet.innerText = depositAmount;
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
+    // Step-4: add numbers to set the total deposit
+    const currentDepositeTotal = previousDepositTotal + newDepositAmount;
+    // set the deposit total 
+    depositTotalElement.innerText = currentDepositeTotal;
+
+    // Step-5: get balance current total 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // Step-6: calculate current total balance 
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    // set the balance total 
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    // Last Step-$ clear the deposit filed 
+    depositField.value = '';
 })
 
