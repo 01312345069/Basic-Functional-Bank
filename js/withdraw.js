9f9010bdaffea8eb940fecc31933c67fdcf91504
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2023-03-18 22:38:48 +0600

    new- Withdraw js  added

---
+++ js/withdraw.js
@@ -0,0 +1,8 @@
+/*
+1. add event handler with the withdraw button
+*/
+
+// step - 1:
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    console.log('WIthdraw button Worked!');
+})
